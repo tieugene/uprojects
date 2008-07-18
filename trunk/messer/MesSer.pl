41f04ac86ca9d6087f4b691f697649bbc601c8cf
slavazanko <slavazanko@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2008-07-18 12:40:48 +0000

    reorganising structure a little bit

--- trunk/messer/MesSer.pl
+++ trunk/messer/MesSer.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 
 
+
 BEGIN {
 #    push @INC, "/usr/local/share/szeng";
     push @INC, "./";
@@ -9,9 +10,11 @@ BEGIN {
 use strict;
 use utf8;
 
+
 use Log::Log4perl;
 use szeng::manager;
 
 my $log = Log::Log4perl->get_logger("MesSer::main");
 $log->info("Останов программы");
+t
 
