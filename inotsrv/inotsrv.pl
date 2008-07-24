8ff36aa36c52acbe41cdae25232b0b786c11ec47
slavazanko <slavazanko@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2008-07-24 07:23:24 +0000

    Концепт для ознакомления

---
+++ inotsrv/inotsrv.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use Data::Dumper::Simple;
+
+
+
+BEGIN {
+#    push @INC, "/usr/local/share/szeng";
+    push @INC, "./";
+    push @INC, "../messer";
+};
+
+                                                                          
