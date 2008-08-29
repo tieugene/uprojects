61442f09993edd04590c1142c74831d36105981c
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2008-08-29 17:14:38 +0000

    qxattr worked ok

--- qxattr/qxattr.py
+++ qxattr/qxattr.py
@@ -6,6 +6,9 @@ import	sys, xattr
 from PyQt4 import QtCore, QtGui
 
 class	MainDialog(QtGui.QDialog):
+	'''
+	Main window.
+	'''
 	def __init__(self, file):
 		self.file = file
 		QtGui.QDialog.__init__(self)
@@ -14,10 +17,10 @@ class	MainDialog(QtGui.QDialog):
 		self.tw = QtGui.QTableWidget(self)
 		self.tw.setColumnCount(2)
 		headerItem = QtGui.QTableWidgetItem()
-		headerItem.setText(QtGui.QApplication.translate("qxattr", "attr", None, QtGui.QApplication.UnicodeUTF8))
+		headerItem.setText(QtGui.QApplication.translate("qxattr", "Attribute", None, QtGui.QApplication.UnicodeUTF8))
 		self.tw.setHorizontalHeaderItem(0,headerItem)
 		headerItem1 = QtGui.QTableWidgetItem()
-		headerItem1.setText(QtGui.QApplication.translate("qxattr", "value", None, QtGui.QApplication.UnicodeUTF8))
+		headerItem1.setText(QtGui.QApplication.translate("qxattr", "Value", None, QtGui.QApplication.UnicodeUTF8))
 		self.tw.setHorizontalHeaderItem(1,headerItem1)
 		self.vbl.addWidget(self.tw)
 		self.hbl = QtGui.QHBoxLayout(self)
@@ -36,37 +39,123 @@ class	MainDialog(QtGui.QDialog):
 		QtCore.QObject.connect(self.delButton, QtCore.SIGNAL("clicked()"),self.__del)
 		self.__load()
 
-	def	__load(self):
-		alist = xattr.listxattr(self.file)
-		if (len(alist)):
-			self.tw.setRowCount(len(alist))
-			for r, a in enumerate(alist):
-				self.tw.setItem(r, 0, QtGui.QTableWidgetItem(a[5:]))
-				self.tw.setItem(r, 1, QtGui.QTableWidgetItem(xattr.getxattr(self.file, a)))
-
 	def	__add(self):
+		'''
+		Add new xattr - new empty line into attr table;
+		'''
 		self.tw.setRowCount(self.tw.rowCount() + 1)
 
 	def	__del(self):
+		'''
+		Delete existent attr.
+		'''
 		r = self.tw.currentRow()
 		if (r >= 0):
 			self.tw.removeRow(r)
 	
+	def	__load(self):
+		'''
+		Load all xattrs into mem.
+		'''
+		self.xattr0 = self.__file2hash(self.file)		# 1. load data
+		self.__hash2table(self.xattr0, self.tw)		# 3. paint
+
 	def	__apply(self):
-		for r in xrange(self.tw.rowCount()):
-			a = str(self.tw.item(r, 0).text())
-			v = str(self.tw.item(r, 1).text())
-			print a, v
-			xattr.setxattr(self.file, "user."+a, v)
+		'''
+		Save attrs and continue edititng.
+		'''
+		h = self.__table2hash(self.tw)			# 1. get new values
+		h1 = self.__file2hash(self.file)		# 2. check that xattrs not changed.
+		#print h, h1
+		if (self.xattr0 != h1):				# FIXME: Force | Reload
+			QtGui.QMessageBox.warning(self,\
+				QtGui.QApplication.translate("qxattr", "Xattrs changed", None, QtGui.QApplication.UnicodeUTF8),\
+				QtGui.QApplication.translate("qxattr", "File attributes changed due editing new. They will be rewrited.", None, QtGui.QApplication.UnicodeUTF8),\
+			)
+		self.__hash2file(h, h1, self.file)
+		self.xattr0 = h1
+
+	def	__file2hash(self, f):
+		'''
+		Load file's xattrs into hash
+		@param f:file
+		@return QHash
+		'''
+		h = {}
+		alist = xattr.listxattr(self.file)
+		for r, a in enumerate(alist):
+			h[a[5:]] = xattr.getxattr(f, a)
+		return h
+
+	def	__hash2table(self, h, t):
+		'''
+		Put hash into table
+		@param h:QHash - source
+		@param t:QTableWidget - dest
+		@return None
+		'''
+		if (len(h)):
+			t.setRowCount(len(h))
+			for r, a in enumerate(h.keys()):
+				t.setItem(r, 0, QtGui.QTableWidgetItem(QtCore.QString().fromUtf8(a)))
+				t.setItem(r, 1, QtGui.QTableWidgetItem(QtCore.QString().fromUtf8(h[a])))
+
+	def	__table2hash(self, t):
+		'''
+		Get data from GUI into inner dict.
+		@param f:QTableWidget
+		@return dict
+		'''
+		h = {}
+		for r in xrange(t.rowCount()):
+			if ((t.item(r, 0) == None) or (t.item(r, 0).text().isEmpty())):		# 1. check empty key
+				QtGui.QMessageBox.critical(self,\
+					QtGui.QApplication.translate("qxattr", "Empty key", None, QtGui.QApplication.UnicodeUTF8),\
+					QtGui.QApplication.translate("qxattr", "Attribute name is empty", None, QtGui.QApplication.UnicodeUTF8),\
+				)
+				return None
+			a = str(t.item(r, 0).text().toUtf8())
+			if h.has_key(a):										# 2. check double keys
+				QtGui.QMessageBox.critical(self,\
+					QtGui.QApplication.translate("qxattr", "Double key", None, QtGui.QApplication.UnicodeUTF8),\
+					QtGui.QApplication.translate("qxattr", "Attribute name already exists", None, QtGui.QApplication.UnicodeUTF8),\
+				)
+				return None
+			if ((t.item(r, 1) == None) or (t.item(r, 1).text().isEmpty())):		# 3. check empty value
+				QtGui.QMessageBox.critical(self,\
+					QtGui.QApplication.translate("qxattr", "Empty value", None, QtGui.QApplication.UnicodeUTF8),\
+					QtGui.QApplication.translate("qxattr", "Attribute value is empty", None, QtGui.QApplication.UnicodeUTF8),\
+				)
+				return None
+			h[a] = str(t.item(r, 1).text().toUtf8())
+		return h
+
+	def	__hash2file(self, h, h1, f):
+		'''
+		Save dict into file's xattrs.
+		@param h:dict new values
+		@param h1:dict old values
+		@param f:file
+		@return None
+		'''
+		# 1. delete deleted
+		for k in h1.keys():
+			if k not in h:
+				xattr.removexattr(f, "user."+k)
+		for r, k in enumerate(h.keys()):
+			#print type(a).__name__, a, type(h[a]).__name__, h[a]
+			if ((k not in h1) or (h[k] != h1[k])):
+				xattr.setxattr(f, "user."+k, h[k])
+				print k, "=", h[k]
 
 if (__name__ == '__main__'):
 	if (len(sys.argv) != 2):
 		print "Usage: %s <filename>" % sys.argv[0]
 		sys.exit(0)
 	aMain = QtGui.QApplication( sys.argv )
-	#translator	= QtCore.QTranslator()
-	#translator.load("tr/ob_" + QtCore.QLocale().system().name().left(2))
-	#aMain.installTranslator(translator)
+	translator	= QtCore.QTranslator()
+	translator.load("tr_" + QtCore.QLocale().system().name().left(2))
+	aMain.installTranslator(translator)
 	mwMain	= MainDialog(sys.argv[1])
 	mwMain.show()
 	sys.exit(aMain.exec_())
