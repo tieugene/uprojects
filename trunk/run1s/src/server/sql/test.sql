888eb1e369962035dc3e149ee225d393373af6d4
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2009-02-17 14:16:04 +0000

    run1s server: deleted unwanted files

--- trunk/run1s/src/server/sql/test.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO user (id, login, password, comments) VALUES (1, "user00", "pass00", "Eugene");
-INSERT INTO org (id, name, comments) VALUES (1, "Org00", "Roga und Kopyta");
-INSERT INTO dbtype (id, name, comments) VALUES (1, "Account", "1C:Accounting 7.7 Typical Pro");
-INSERT INTO host (id, name) VALUES (1, "server");
-INSERT INTO share (id, hostid, name) VALUES (1, 1, "1c");
-INSERT INTO db (id, shareid, dbtypeid, orgid, path, comments) VALUES (1, 1, 1, 1, "A.Roga", "encaching");
-INSERT INTO acl (id, userid, dbid, visible) VALUES (1, 1, 1, "T");
