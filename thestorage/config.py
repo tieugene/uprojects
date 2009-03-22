859f9da005135ba86bf5eeed4e16eeb15d8086d9
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2009-03-22 12:41:40 +0000

    thestorage: fixed bugs, made mime and datetime editting; download files.

--- thestorage/config.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-dbfn = "thestorage.db"
--- thestorage/src/soft.py
+++ thestorage/src/soft.py
@@ -1,19 +1,57 @@
 # -*- coding: utf-8 -*-
 '''
-Software
-from xdg import Mime
-import mimetypes
-Mime.get_type(<filename>)	# > .media, .subtype
-a = Mime.lookup('application/vnd.ms-excel')
-a.get_comments()
-? Mime.exts
 '''
+
 
 from __future__ import with_statement
 from xdg import Mime
 import sys, os, re, hashlib, tempfile
 import web
 import var, config
+
+
+def	date2tuple(d):
+	'''
+	Converts date in form of int timestamp into tuple of values.
+	Unused values replaced w/ None.
+	E.g. 2000 => (2000, None, ...), 200809 => (2008, 9, None, ...) etc
+	@param d:int - date
+	@return (y, m, d, h, m, s)
+	'''
+	d_str = str(d)
+	year	= int(d_str[:4]) if (d) else None
+	month	= int(d_str[4:6]) if (year) and (len(d_str) > 4) else None
+	day	= int(d_str[6:8]) if (month) and (len(d_str) > 6) else None
+	hour	= int(d_str[8:10]) if (day) and (len(d_str) > 8) else None
+	minute	= int(d_str[10:12]) if (hour) and (len(d_str) > 10) else None
+	second	= int(d_str[12:14]) if (minute) and (len(d_str) > 12) else None
+	return (year, month, day, hour, minute, second)
+
+
+def	tuple2date(year, month, day, hour, minute, second):
+	'''
+	Converts date in form of tuple into timestamp as int.
+	E.g. (2000, -1, ...) => 2000, (2008, 9, -1, ...) => 200809  etc
+	@param (y, m, d, h, m, s)
+	@return int
+	'''
+	datetime = ""
+	tmp = "%04d" % year if (year >= 0) else ""
+	datetime += tmp
+	tmp = "%02d" %  month if (tmp) and (month >= 0) else ""
+	datetime += tmp
+	tmp = "%02d" % day if (tmp) and (day >= 0) else ""
+	datetime += tmp
+	tmp = "%02d" % hour if (tmp) and (hour >= 0) else ""
+	datetime += tmp
+	tmp = "%02d" % minute if (tmp) and (minute >= 0) else ""
+	datetime += tmp
+	tmp = "%02d" % second if (tmp) and (second >= 0) else ""
+	datetime += tmp
+	if len(datetime) == 0:
+		datetime = None
+	return datetime
+
 
 class	MimeHelper:
 	'''
@@ -74,8 +112,20 @@ class	main:
 		if (view == 'list'):
 			items = var.mydb.select('softview')
 			return self.listform(var.root, items)
+		elif (view == 'download'):
+			item = var.mydb.select('softview', where='id=%s' % id)[0]
+			web.header("Content-Type", "%s/%s;" % (item.mime_media, item.mime_type))
+			web.header("Content-Transfer-Encoding" , "binary"); 
+			web.header("Content-Disposition", "attachment; filename=\"%s\";" % item.origfn);
+			web.header("Content-Length", "%d" % item.size); 
+			return open(os.path.join(config.filepath, "%08X" % (int(item.id)))).read()
+		elif (view == 'view'):
+			item = var.mydb.select('softview', where='id=%s' % id)[0]
+			return self.viewform(var.root, item)
+		elif (view == 'edit'):
+			item = var.mydb.select('softview', where='id=%s' % id)[0]
+			return self.editform(var.root, item, var.mydb, date2tuple(item.datetime))
 		elif (view == 'del'):
-			ext = var.mydb.select('file', where='id=%s' % id)[0].ext
 			t = var.mydb.transaction()
 			try:
 				n = var.mydb.delete('object', where='id=%s' % id)
@@ -88,18 +138,9 @@ class	main:
 				var.message = '%d programm deleted ok' % n
 				success = True
 			if (success):
-				filename = os.path.join(config.filepath, "%08X.%s" % (int(id), ext))
+				filename = os.path.join(config.filepath, "%08X" % (int(id)))
 				os.remove(filename)
 			raise web.seeother(self.mainlistname)
-		elif (view == 'view'):
-			item = var.mydb.select('softview', where='id=%s' % id)[0]
-			return self.viewform(var.root, item)
-		elif (view == 'edit'):
-			item = var.mydb.select('softview', where='id=%s' % id)[0]
-			platform = var.mydb.select('platform')
-			vendor = var.mydb.select('vendor')
-			distrib = var.mydb.select('distrib')
-			return self.editform(var.root, item, platform, vendor, distrib)
 		else:
 			var.message = 'Unknown action'
 			raise web.seeother(self.mainlistname)
@@ -124,10 +165,10 @@ class	main:
 				f.write(x.value)
 			# 4. get mime, extension, hash
 			newmimetype = Mime.get_type(tmpfn)
-			tmpext = mimehelper.getext(newmimetype)
-			newext = tmpext[0] if tmpext else ""
+			mimeid = int(var.mydb.select('mimesubtype', where='name="%s"' % newmimetype.subtype)[0].id) # FIXME: if none
 			# 5. try add record to object => id
 			t = var.mydb.transaction()
+			#if (True):
 			try:
 				newid = var.mydb.insert('object')
 				# 6. mk filename ("%08X" % id)
@@ -136,13 +177,12 @@ class	main:
 					id = newid,
 					size = os.path.getsize(tmpfn),
 					md5 = newmd5,
-					mimetype = "%s/%s" % (newmimetype.media, newmimetype.subtype),
-					origfn = x.filename,
-					ext = newext)
+					mime = mimeid,
+					origfn = x.filename)
 				var.mydb.insert(self.dbname,
 					id = newid)
 				# 7. try rename file
-				os.rename(tmpfn, os.path.join(config.filepath, newfn + "." + newext))
+				os.rename(tmpfn, os.path.join(config.filepath, newfn))
 			except:
 				t.rollback()
 				os.remove(tmpfn)
@@ -152,6 +192,7 @@ class	main:
 				raiseform = "/soft/main/edit/%d" % newid
 		elif (view == 'edit'):
 			i = web.input()
+			dt = tuple2date(int(i.year), int(i.month), int(i.day), int(i.hour), int(i.minute), int(i.second))
 			t = var.mydb.transaction()
 			try:
 				var.mydb.update(self.dbname, where="id=%s" % id,
@@ -161,7 +202,8 @@ class	main:
 					name=i.name,
 					ver=i.ver)
 				var.mydb.update('file', where="id=%s" % id,
-					mimetype=i.mimetype,
+					datetime=dt,
+					mime=i.mime,
 					origfn=i.origfn)
 				var.mydb.update('object', where="id=%s" % id,
 					comments=i.comments)
