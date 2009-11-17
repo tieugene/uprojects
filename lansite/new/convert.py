8cc16eae4aae417ff927bfa690f6f37155e465c3
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2009-11-17 10:54:38 +0000

    new sro

---
+++ lansite/new/convert.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+import json, pprint
+
+# 1. load data
+data = json.load(open('sro.json', 'r'))
+permitown = list()
+for i, a in enumerate(data):
+	if a['model'] == 'sro.permit':
+# 2. create new permitowns
+		permitown.append({
+			'pk':		a['pk'],
+			'model':	'sro.permitown',
+			'fields':	{
+				'permit':	a['pk'],
+				'date':		a['fields']['date'],
+				'regno':	a['fields']['regno'],
+				'meeting':	a['fields']['meeting']
+			}
+		})
+# 3. tune old permit:
+# 3.1. remove unwanted fields
+		del data[i]['fields']['date']
+		del data[i]['fields']['regno']
+		del data[i]['fields']['meeting']
+# 3.2. add type
+		data[i]['fields']['permittype'] = 1
+# 4. add permittype
+data.extend([
+	{ 'pk': 1, 'model': 'sro.permittype', 'fields': { 'id':	1, 'name': 'Permition' } },
+	{ 'pk': 2, 'model': 'sro.permittype', 'fields': { 'id':	2, 'name': 'Statement' } },
+	{ 'pk': 3, 'model': 'sro.permittype', 'fields': { 'id':	3, 'name': 'Alient permition' } },
+])
+# 5. merge data
+data.extend(permitown)
+# 6. flush
+json.dump(data, open('sro_new.json', 'w'), indent=1)
