3fe3bf89c51fda03a64eae2606e9d764642ceebb
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2010-04-05 15:58:06 +0000

    some changes

--- lansite/usr/share/lansite/gw/forms.py
+++ lansite/usr/share/lansite/gw/forms.py
@@ -23,3 +23,16 @@ class	AssignCatForm(forms.ModelForm):
 	class	Meta:
 		model = AssignCat
 
+class	AssignForm(forms.ModelForm):
+	class	Meta:
+		model = Assign
+		fields = ('deadline', 'subject', 'description', 'category', 'importance', 'assignee')
+
+class	UserListForm(forms.Form):
+	user	= forms.ModelChoiceField(queryset=GwUser.objects.all(), required=True)
+
+class	LineCommentForm(forms.Form):
+	comment	= forms.CharField(label='Причина')
+
+class	AssignDupForm(forms.Form):
+	assign	= forms.ModelChoiceField(queryset=Assign.objects.all(), required=True)
--- lansite/usr/share/lansite/gw/urls.py
+++ lansite/usr/share/lansite/gw/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
+from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('lansite.gw.views',
 	(r'^$',					'index'),
@@ -32,17 +33,18 @@ urlpatterns = patterns('lansite.gw.views',
 	(r'^assignc/edit/(?P<item_id>\d+)/$',	'assigncat_edit'),
 	(r'^assignc/del/(?P<item_id>\d+)/$',	'assigncat_del'),
 
-	(r'^assign/list/$',				'assign_list'),
-	(r'^assign/add/$',				'assign_add'),
-	(r'^assign/view/(?P<item_id>\d+)/$',		'assign_view'),
-	(r'^assign/edit/(?P<item_id>\d+)/$',		'assign_edit'),
-	(r'^assign/del/(?P<item_id>\d+)/$',		'assign_del'),
+	(r'^assign/list/$',			'assign_list'),
+	(r'^assign/add/$',			'assign_add'),
+	(r'^assign/view/(?P<item_id>\d+)/$',	'assign_view'),
+	(r'^assign/edit/(?P<item_id>\d+)/$',	'assign_edit'),
+	(r'^assign/del/(?P<item_id>\d+)/$',	'assign_del'),
 	(r'^assign/route/(?P<item_id>\d+)/$',	'assign_route'),
 	(r'^assign/invalid/(?P<item_id>\d+)/$',	'assign_invalid'),
 	(r'^assign/duped/(?P<item_id>\d+)/$',	'assign_duped'),
 	(r'^assign/accept/(?P<item_id>\d+)/$',	'assign_accept'),
-	(r'^assign/done/(?P<item_id>\d+)/$',		'assign_done'),
+	(r'^assign/done/(?P<item_id>\d+)/$',	'assign_done'),
 	(r'^assign/approve/(?P<item_id>\d+)/$',	'assign_approve'),
 	(r'^assign/reopen/(?P<item_id>\d+)/$',	'assign_reopen'),
 	(r'^assign/mkdep/(?P<item_id>\d+)/$',	'assign_mkdep'),
+	(r'^assign/history/(?P<item_id>\d+)/$',	'assign_history'),
 )
