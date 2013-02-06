5805a30de3e243ac5f6eb1abd45e5fc2c504b15e
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-02-06 13:14:07 +0000

    person create ok

--- cms/cmsdj/core/urls.py
+++ cms/cmsdj/core/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'core.views.index', name='core_index'),
-    url(r'^person/$', 'core.views.person_list', name='person_list'),
-    url(r'^person/(?P<id>\d+)/$',	   views.person_detail, name='person_detail'),
+    url(r'^$',                          views.index, name='core_index'),
+    url(r'^person/$',                   views.person_list, name='person_list'),
+    url(r'^person/c/$',	                views.person_create, name='person_create'),
+    url(r'^person/(?P<id>\d+)/r/$',	    views.person_detail, name='person_detail'),
+    url(r'^person/(?P<id>\d+)/sd/$',	views.person_delete, name='person_delete'),
+    url(r'^person/(?P<id>\d+)/um/$',    views.person_update_main, name='person_update_main'),
 )
--- cms/cmsdj/core/views.py
+++ cms/cmsdj/core/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.simple import direct_to_template
 from django.views.generic.list_detail import object_list, object_detail
+from django.views.generic.create_update import create_object, update_object, delete_object
 
 import models
 
@@ -11,6 +13,7 @@ PAGE_SIZE = 20
 def index(request):
     return direct_to_template(request, 'core/index.html')
 
+# Person CRUD
 def person_list(request):
 	return  object_list (
 		request,
@@ -19,7 +22,25 @@ def person_list(request):
 		page = int(request.GET.get('page', '1')),
 	)
 
+@csrf_exempt
+def person_create(request):
+	return  create_object (
+		request,
+        model = models.Person,
+        template_name = 'core/person_main_form.html',
+	)
+
+def person_delete(request, id):
+    pass
+
 def person_detail(request, id):
+	return  object_detail (
+		request,
+		queryset = models.Person.objects.all(),
+		object_id = id,
+	)
+
+def person_update_main(request, id):
 	return  object_detail (
 		request,
 		queryset = models.Person.objects.all(),
