90acf712de4641ce3df8ff56830d09367d99b3ca
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-02-06 17:56:00 +0000

    person: add/update/delete ok

--- cms/cmsdj/core/urls.py
+++ cms/cmsdj/core/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     url(r'^person/$',                   views.person_list, name='person_list'),
     url(r'^person/c/$',	                views.person_create, name='person_create'),
     url(r'^person/(?P<id>\d+)/r/$',	    views.person_detail, name='person_detail'),
-    url(r'^person/(?P<id>\d+)/sd/$',	views.person_delete, name='person_delete'),
-    url(r'^person/(?P<id>\d+)/um/$',    views.person_update_main, name='person_update_main'),
+    url(r'^person/(?P<id>\d+)/d/$',     views.person_delete, name='person_delete'),
+    url(r'^person/(?P<id>\d+)/u/$',     views.person_update, name='person_update'),
 )
