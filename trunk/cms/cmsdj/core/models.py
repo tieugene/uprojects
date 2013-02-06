f21f808e59d7079bbafe136d24b83e192561e1da
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-02-06 19:03:18 +0000

    person: address CUD starting

--- trunk/cms/cmsdj/core/models.py
+++ trunk/cms/cmsdj/core/models.py
@@ -44,7 +44,7 @@ class   PersonAddress(models.Model):
     app         = models.CharField(max_length=4, verbose_name=u'Квартира')
 
     def     __unicode__(self):
-        return self.no
+        return 'д. %s, корп. %s, стр. %s, кв. %s' % (self.no, self.housing, self.building, self.app)
 
     class   Meta:
         unique_together         = (('person', 'addrtype',),)
