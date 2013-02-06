4e08e23b80d32ba34d361c43a7a6f5b9d121f733
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-02-06 19:32:46 +0000

    some tunings

--- trunk/cms/cmsdj/core/views.py
+++ trunk/cms/cmsdj/core/views.py
@@ -8,7 +8,7 @@ from django.views.generic.simple import direct_to_template
 from django.views.generic.list_detail import object_list, object_detail
 from django.views.generic.create_update import create_object, update_object, delete_object
 
-import models
+import models, forms
 
 PAGE_SIZE = 20
 
@@ -37,18 +37,24 @@ def person_delete(request, id):
     return redirect('person_list')
 
 def person_create(request):
-	return  create_object (request, model = models.Person)
+	return  create_object (request, model = models.Person, extra_context = {'cancelurl': reverse('person_list')})
 
 def person_update(request, id):
-	return  update_object (request, model = models.Person, object_id = id, extra_context = {'next': reverse('person_detail', args=[id,])})
+	return  update_object (request, model = models.Person, object_id = id, extra_context = {'cancelurl': reverse('person_detail', args=[id,])})
 
 # PersonAddress
 def personaddress_delete(request, id):
-    models.PersonAddress.objects.get(pk=int(id)).delete()
-    return redirect('person_detail')
+    object = models.PersonAddress.objects.get(pk=int(id))
+    person = object.person
+    object.delete()
+    return redirect('person_detail', args=[person.pk,])
 
 def personaddress_create(request, id):
-	return  create_object (request, model = models.PersonAddress)
+    person = models.Person.objects.get(pk=int(id))
+    #return  create_object (request, model = models.PersonAddress)
+    return  create_object (request, form_class = forms.PersonAddressForm, extra_context = {'cancelurl': reverse('person_detail', args=[person.pk,])})
 
 def personaddress_update(request, id):
-	return  update_object (request, model = models.PersonAddress, object_id = id, extra_context = {'next': reverse('person_detail', args=[id,])})
+    object = models.PersonAddress.objects.get(pk=int(id))
+    person = object.person
+    return  update_object (request, model = models.PersonAddress, object_id = id, extra_context = {'cancelurl': reverse('person_detail', args=[person.pk,])})
