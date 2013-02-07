a999cffe50a248186468ae810cd8d7bb7749b41f
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-02-07 18:49:30 +0000

    patient: ready to start (model, admin, base urls, base html)

--- cms/cmsdj/patient/admin.py
+++ cms/cmsdj/patient/admin.py
@@ -3,7 +3,37 @@
 from django.contrib import admin
 from models import *
 
-class	PatientAdmin(admin.ModelAdmin):
-    pass
+# 1. inlines
+class   MedHistoryLine(admin.TabularInline):
+    model   = MedHistory
+    extra   = 1
 
-admin.site.register(Patient)
+class   VisitLine(admin.TabularInline):
+    model   = Visit
+    extra   = 1
+
+class   MedRecordLine(admin.TabularInline):
+    model   = MedRecord
+    extra   = 1
+
+class   MedRecordEntryLine(admin.TabularInline):
+    model   = MedRecordEntry
+    extra   = 1
+
+# 2. ordinar
+class	PatientAdmin(admin.ModelAdmin):
+    inlines = (MedHistoryLine,)
+
+class	MedHistoryAdmin(admin.ModelAdmin):
+    inlines = (VisitLine,)
+
+class	VisitAdmin(admin.ModelAdmin):
+    inlines = (MedRecordLine,)
+
+class	MedRecordAdmin(admin.ModelAdmin):
+    inlines = (MedRecordEntryLine,)
+
+admin.site.register(Patient,    PatientAdmin)
+admin.site.register(MedHistory, MedHistoryAdmin)
+admin.site.register(Visit,      VisitAdmin)
+admin.site.register(MedRecord,  MedRecordAdmin)
