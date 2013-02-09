58facaed0fe71f23e4126e8d8c4fd6e643e7dd2a
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-02-09 14:36:13 +0000

    ref/pmu ok (draft)

--- cms/cmsdj/ref/models.py
+++ cms/cmsdj/ref/models.py
@@ -3,6 +3,43 @@
 from django.db import models
 from django.conf import settings
 
+class   PMU1(models.Model):
+    name    = models.CharField(max_length=255, unique=True, verbose_name=u'Наименование')
+
+    def     __unicode__(self):
+        return '%02d %s' % (self.pk, self.name)
+
+    class   Meta:
+        ordering                = ('name', )
+        verbose_name            = u'ПМУ (тип)'
+        verbose_name_plural     = u'ПМУ (типы)'
+
+class   PMU2(models.Model):
+    name    = models.CharField(max_length=255, unique=True, verbose_name=u'Наименование')
+
+    def     __unicode__(self):
+        return '%02d %s' % (self.pk, self.name)
+
+    class   Meta:
+        ordering                = ('name', )
+        verbose_name            = u'ПМУ (класс)'
+        verbose_name_plural     = u'ПМУ (классы)'
+
+class   PMU3(models.Model):
+    c1      = models.ForeignKey(PMU1, related_name='items', verbose_name=u'Тип')
+    c2      = models.ForeignKey(PMU2, related_name='items', verbose_name=u'Класс')
+    c3      = models.PositiveIntegerField(verbose_name=u'Код')
+    name    = models.CharField(max_length=255, verbose_name=u'Наименование')
+
+    def     __unicode__(self):
+        return '%02d.%02d.%03d %s' % (self.c1.pk, self.c2.pk, self.c3, self.name)
+
+    class   Meta:
+        ordering                = ('c1', 'c2', 'c3',)
+        unique_together         = ('c1', 'c2', 'c3',)
+        verbose_name            = u'ПМУ'
+        verbose_name_plural     = u'ПМУ'
+'''
 class   MKB10(models.Model):
     name    = models.CharField(max_length=128, unique=True, verbose_name=u'Наименование')
 
@@ -13,3 +50,4 @@ class   MKB10(models.Model):
         ordering                = ('name', )
         verbose_name            = u'Код МКБ-10'
         verbose_name_plural     = u'Коды МКБ-10'
+'''
--- cms/cmsdj/ref/urls.py
+++ cms/cmsdj/ref/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'ref.views.index', name='ref_index'),
-    url(r'^mkb10/$', 'app1.views.mkb10_list', name='mkb10_list'),
-    url(r'^mkb10/(?P<id>\d+)/$',	   views.mkb10_detail, name='mkb10_detail'),
+    url(r'^$',          views.index, name='ref_index'),
+    url(r'^pmu/$',      views.pmu_list, name='pmu_list'),
 )
