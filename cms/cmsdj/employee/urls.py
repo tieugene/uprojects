9f26f71db2be6c47c47a17fb16ff5c12aa23e91b
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-02-21 17:21:15 +0000

    stafflist starts

--- cms/cmsdj/employee/urls.py
+++ cms/cmsdj/employee/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'employee.views.index', name='employee_index'),
+    url(r'^$', views.index, name='employee_index'),
+    url(r'^stafflist/$', views.stafflist, name='stafflist'),
     url(r'^employee/$', 'employee.views.employee_list', name='employee_list'),
     url(r'^employee/(?P<id>\d+)/$',	   views.employee_detail, name='employee_detail'),
 )
