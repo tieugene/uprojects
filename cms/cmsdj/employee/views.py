95d4a3697f9ad667adf30ac6d15380bcb0903625
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-02-22 18:01:43 +0000

    stafflistentry CRUD ok

--- cms/cmsdj/employee/views.py
+++ cms/cmsdj/employee/views.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.db.models import Sum
-from django.shortcuts import render_to_response
+from django.shortcuts import redirect, render_to_response
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.simple import direct_to_template
 from django.views.generic.list_detail import object_list, object_detail
+from django.views.generic.create_update import create_object, update_object, delete_object
 
-import models
+import models, forms
 
 PAGE_SIZE = 20
 
@@ -29,7 +32,11 @@ def stafflist_view(request, id):
     '''
     # 1. load wanted (StaffListEntry => specialty.id: qty)
     sl = models.StaffList.objects.get(pk=int(id))
-    need = dict(sl.staves.values_list('specialty_id', 'qty'))
+    need = dict()
+    for i in sl.staves.all():
+        #print i
+        need[i.specialty.id] = i    # Specialty.id: StaffListEntry
+    #need = dict(sl.staves.values_list('specialty_id', 'qty'))
     # 2. load exists (EmployeeSpecialty => specialty.id, Sum(rate)
     #are = models.EmployeeSpecialty.objects.values('specialty_id')  #.annotate(total=Sum('rate')).order_by()
     are = {}
@@ -41,18 +48,79 @@ def stafflist_view(request, id):
     for department in models.Department.objects.all():
         subdata = list()
         for specialty in department.specialties.all():
-            subdata.append({'name': specialty.name, 'need': need.get(specialty.id, '-'), 'are': are.get(specialty.id, '-')})
-        data.append({'name': department.name, 'data': subdata})
-    return render_to_response('employee/stafflist_detail.html', {'begdate': sl.begdate, 'data': data})
+            entry = {'spec': specialty}
+            if specialty.id in need:
+                entry['need'] = need[specialty.id]
+            if specialty.id in are:
+                entry['are'] = are[specialty.id]
+            subdata.append(entry)
+        data.append({'dep': department, 'data': subdata})
+    return render_to_response('employee/stafflist_detail.html', {'stafflist': sl, 'data': data})
 
-def staff_add(request, id):
-    pass
+@csrf_exempt
+def staff_add(request, list_id, spec_id):
+    sl = models.StaffList.objects.get(pk=int(list_id))
+    spec = models.Specialty.objects.get(pk=int(spec_id))
+    if request.method == 'POST':
+        form = forms.StaffForm(request.POST)
+        if form.is_valid():
+            models.StaffListEntry.objects.create(stafflist=sl, specialty=spec, qty=form.cleaned_data['qty'])
+            return redirect('stafflist_view', list_id)
+    else:
+        form=forms.StaffForm()
+    return render_to_response(
+        'employee/staff_add.html',
+        {
+            'form': form,
+            'stafflist': sl,
+            'spec': spec,
+        }
+    )
 
+@csrf_exempt
 def staff_edit(request, id):
-    pass
+    sle = models.StaffListEntry.objects.get(pk=int(id))
+    if request.method == 'POST':
+        form = forms.StaffForm(request.POST)
+        if form.is_valid():
+            sle.qty = form.cleaned_data['qty']
+            sle.save()
+            return redirect('stafflist_view', sle.stafflist.pk)
+    else:
+        form=forms.StaffForm(initial={'qty': sle.qty})
+    return render_to_response(
+        'employee/staff_edit.html',
+        {
+            'form': form,
+            'sle': sle,
+        }
+    )
 
 def staff_del(request, id):
-    pass
+    staff = models.StaffListEntry.objects.get(pk=int(id))
+    sl = staff.stafflist
+    staff.delete()
+    return redirect('stafflist_view', sl.pk)
+
+def roomschedule_list(request):
+	return  object_list (
+		request,
+		queryset = models.RoomSchedule.objects.order_by('begdate'),
+	)
+
+def roomschedule_view(request, id):
+    rs = models.RoomSchedule.objects.get(pk=int(id))
+    return render_to_response('employee/roomschedule_detail.html', {'begdate': rs.begdate})
+
+def rse_add(request, id):
+    return  create_object (request, model = models.Person, extra_context = {'cancelurl': reverse('person_list')})
+
+def rse_edit(request, id):
+    return  update_object (request, model = models.Person, object_id = id, extra_context = {'cancelurl': reverse('person_detail', args=[id,])})
+
+def rse_del(request, id):
+    models.Person.objects.get(pk=int(id)).delete()
+    return redirect('person_list')
 
 def employee_list(request):
 	return  object_list (
