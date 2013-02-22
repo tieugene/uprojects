6c4df1967d55f82f915c72976e26037a146912e8
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-02-22 15:28:46 +0000

    stafflist list/detail ok

--- cms/cmsdj/employee/urls.py
+++ cms/cmsdj/employee/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='employee_index'),
-    url(r'^stafflist/$', views.stafflist, name='stafflist'),
-    url(r'^employee/$', 'employee.views.employee_list', name='employee_list'),
-    url(r'^employee/(?P<id>\d+)/$',	   views.employee_detail, name='employee_detail'),
+    url(r'^$',                              views.index,            name='employee_index'),
+    url(r'^stafflist/$',                    views.stafflist_list,   name='stafflist_list'),
+    url(r'^stafflist/(?P<id>\d+)/$',        views.stafflist_view,   name='stafflist_view'),
+    url(r'^staff/(?P<id>\d+)/add/$',        views.staff_add,        name='staff_add'),
+    url(r'^staff/(?P<id>\d+)/edit/$',       views.staff_edit,       name='staff_edit'),
+    url(r'^staff/(?P<id>\d+)/del/$',        views.staff_del,        name='staff_del'),
+    url(r'^employee/$',                     views.employee_list,    name='employee_list'),
+    url(r'^employee/(?P<id>\d+)/$',	        views.employee_view,    name='employee_view'),
 )
--- cms/cmsdj/employee/views.py
+++ cms/cmsdj/employee/views.py
@@ -13,7 +13,13 @@ PAGE_SIZE = 20
 def index(request):
     return direct_to_template(request, 'employee/index.html')
 
-def stafflist(request):
+def stafflist_list(request):
+	return  object_list (
+		request,
+		queryset = models.StaffList.objects.order_by('begdate'),
+	)
+
+def stafflist_view(request, id):
     '''
     По всем отделениям
     По всем специальностям оного
@@ -22,7 +28,8 @@ def stafflist(request):
     => тупо заполнить список ((dep, (spec, qty, ready)), )
     '''
     # 1. load wanted (StaffListEntry => specialty.id: qty)
-    need = dict(models.StaffList.objects.all()[0].staves.values_list('specialty_id', 'qty'))
+    sl = models.StaffList.objects.get(pk=int(id))
+    need = dict(sl.staves.values_list('specialty_id', 'qty'))
     # 2. load exists (EmployeeSpecialty => specialty.id, Sum(rate)
     #are = models.EmployeeSpecialty.objects.values('specialty_id')  #.annotate(total=Sum('rate')).order_by()
     are = {}
@@ -36,7 +43,16 @@ def stafflist(request):
         for specialty in department.specialties.all():
             subdata.append({'name': specialty.name, 'need': need.get(specialty.id, '-'), 'are': are.get(specialty.id, '-')})
         data.append({'name': department.name, 'data': subdata})
-    return render_to_response('employee/stafflist_detail.html', {'data': data})
+    return render_to_response('employee/stafflist_detail.html', {'begdate': sl.begdate, 'data': data})
+
+def staff_add(request, id):
+    pass
+
+def staff_edit(request, id):
+    pass
+
+def staff_del(request, id):
+    pass
 
 def employee_list(request):
 	return  object_list (
@@ -46,7 +62,7 @@ def employee_list(request):
 		page = int(request.GET.get('page', '1')),
 	)
 
-def employee_detail(request, id):
+def employee_view(request, id):
 	return  object_detail (
 		request,
 		queryset = models.Employee.objects.all(),
