fbe5747dea6029817e80079054d3c0c9e25493d2
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-03-04 10:07:12 +0000

    some trunks moved to tags

--- cms/cmsdj/enum/sql/gender.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO enum_gender (id, name) VALUES (1, 'муж');
-INSERT INTO enum_gender (id, name) VALUES (2, 'жен');
--- cms/cmsdj/enum/sql/personcodetype.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO enum_personcodetype (id, name) VALUES (1, 'ИНН');
-INSERT INTO enum_personcodetype (id, name) VALUES (2, 'СНИЛС');
-INSERT INTO enum_personcodetype (id, name) VALUES (3, 'ЕНП');
