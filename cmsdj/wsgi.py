4597a6ece6fbe5fdec5ff019fee52c2dca8c076a
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-03-05 11:25:45 +0000

    rseform: msked input

--- cmsdj/wsgi.py
+++ cmsdj/wsgi.py
@@ -1,4 +1,5 @@
-import os
+import os, sys
+sys.path.append(os.path.dirname(__file__))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
