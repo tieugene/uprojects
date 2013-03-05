2cc2d65be68ff3d9dc029bf0536c9fed7fc94afc
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-03-05 16:26:25 +0000

    flush

--- trunk/cms/cmsdj/enum/sql/persondoctype.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO enum_persondoctype (id, name) VALUES (1, 'паспорт гражданина РФ');
-INSERT INTO enum_persondoctype (id, name) VALUES (2, 'вид на жительство');
-INSERT INTO enum_persondoctype (id, name) VALUES (3, 'иностранный паспорт');
-INSERT INTO enum_persondoctype (id, name) VALUES (4, 'водительское удостоверение');
