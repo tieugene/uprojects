0116ea9175645af761b0c9b33a0221daee41d5ba
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-03-19 17:26:37 +0000

    patient paged ok

--- cmsdj/patient/views.py
+++ cmsdj/patient/views.py
@@ -3,14 +3,14 @@
 from django.conf import settings
 #from django.views.generic.simple import direct_to_template
 #from django.views.generic.list_detail import object_list, object_detail
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from jnj import *
+from utils.pager import page_queryset
 import models
 
-PAGE_SIZE = 20
-
 def patient_list(request):
-    return jrender_to_response('patient/patient_list.html', {'object_list': models.Patient.objects.order_by('pk'),}, request=request)
+    return jrender_to_response('patient/patient_list.html', { 'object_list': page_queryset(models.Patient.objects.all(), request.GET.get('page', 1)), }, request=request)
 
 def patient_detail(request, id):
     return jrender_to_response('patient/patient_detail.html', {'object': models.Patient.objects.get(pk=int(id))}, request=request)
