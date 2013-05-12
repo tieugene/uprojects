d6f4bb4ec33dd2b943ad10f7df0091f1fdb8399b
ti.eugene <ti.eugene@9e15c518-1b2f-0410-9571-c58ee19b2d78>
2013-05-12 17:26:00 +0000

    clean trunk/cms* stuff

--- cmsdj/ref/sql/skmu1.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO ref_skmu1 (id, name) VALUES (1, 'УСЛУГИ ВРАЧА ЛЕЧЕБНО - ДИАГНОСТИЧЕСКИЕ');
-INSERT INTO ref_skmu1 (id, name) VALUES (2, 'УСЛУГИ СЕСТРИНСКОГО УХОДА');
-INSERT INTO ref_skmu1 (id, name) VALUES (3, 'КОМПЛЕКСНЫЕ ДИАГНОСТИЧЕСКИЕ УСЛУГИ (МЕТОДЫ ИССЛЕДОВАНИЯ: ЛАБОРАТОРНЫЕ, ФУНКЦИОНАЛЬНЫЕ, ИНСТРУМЕНТАЛЬНЫЕ, РЕНТГЕНОРАДИОЛОГИЧЕСКИЕ И ДР.)');
-INSERT INTO ref_skmu1 (id, name) VALUES (4, 'МЕДИЦИНСКИЕ УСЛУГИ ПО ПРОФИЛАКТИКЕ');
-INSERT INTO ref_skmu1 (id, name) VALUES (5, 'МЕДИЦИНСКИЕ УСЛУГИ ПО РЕАБИЛИТАЦИИ');
--- cmsdj/views.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf import settings
-from jnj import *
-
-def index(request):
-    return jrender_to_response('index.html', request=request)
